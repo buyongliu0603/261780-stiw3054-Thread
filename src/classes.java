1d05924376ceb40934bc746bfab9b0a6cf36365b
buyongliu0603 <60844314+buyongliu0603@users.noreply.github.com>
2020-12-31 09:24:10 +0800

    Add files via upload

---
+++ src/classes.java
@@ -0,0 +1,32 @@
+public class classes {
+}
+class classA extends Runnable{
+    public void run(){
+
+    }}
+
+class classB extends Object{
+    public void run(){
+
+    }
+}
+class classC implements Runnable{
+    public void run(){
+
+    }
+}
+class classD implements Runnable{
+    void run(){}
+
+}
+ class classE implements Runnable{
+
+    public void start() {
+
+    }
+}
+
+
+
+
+
